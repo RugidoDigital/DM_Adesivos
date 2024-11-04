682defe477f14279a69ae65ef3ba8e2e14eb529f
RugidoDigital <agencia.rugidodigital@gmail.com>
2024-11-04 16:45:48 -0400

    Teste de passagem de rota

---
+++ js/redirect.js
@@ -0,0 +1,13 @@
+const path = window.location.pathname;
+if (path === "/index.html") {
+    window.location.replace("/");
+}
+if (path === "/item.html") {
+    window.location.replace("/item");
+}
+// if (path === "/carrinho.html") {
+//     window.location.replace("./carrinho");
+// }
+// if (path === "/fazerPedido.html") {
+//     window.location.replace("./fazerPedido");
+// }
