dc44d3350ef85f53a5b4e47e9ce669a9a28fbcda
RugidoDigital <agencia.rugidodigital@gmail.com>
2024-11-04 17:11:47 -0400

    Ajusted Route Page Index

--- js/redirect.js
+++ js/redirect.js
@@ -2,12 +2,3 @@ const path = window.location.pathname;
 if (path === "/index.html") {
     window.location.replace("/");
 }
-if (path === "/item.html") {
-    window.location.replace("/item");
-}
-// if (path === "/carrinho.html") {
-//     window.location.replace("./carrinho");
-// }
-// if (path === "/fazerPedido.html") {
-//     window.location.replace("./fazerPedido");
-// }
